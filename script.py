e78e4b6b87a3f57ae5953c2ced5e9a62c91cfcb9
SSloan07 <159721122+SSloan07@users.noreply.github.com>
2026-07-25 17:16:58 -0500

    Script orchestation updated in order to use valid_type function

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-from .alphabet import (
+from .Utils.alphabet import (
     alphabet_spanish,
     alphabet_spanish_inv,
     alphabet_english,
@@ -7,47 +7,40 @@ from .alphabet import (
 
 from .Core.encryption import encryption
 from .Core.decryption import decryption
-
+from .Utils.invalid_type import valid_type
 
 print("Welcome to our Caesar Cipher")
 
 while True:
-    # Info from usser
-    option = int(input("\nMenu\n1) Encryption\n2) Decryption\nChoose an option: "))
-    language_option = int(input("\nChoose a language for the message\n1) Spanish\n2) English\nChoose an option: "))
+    # Info from user
+
+    option = valid_type("\nMenu\n1) Encryption\n2) Decryption\nChoose an option: ",(1, 2))
+    language_option = valid_type("\nChoose a language\n1) Spanish\n2) English\nChoose an option: ",(1, 2))
     message = input("\nIntroduce your message: ")
-    key = int(input("\nIntroduce your key: "))
+    key = valid_type("\nIntroduce your key: ")
+
 
     # Validate alphabet
     if language_option == 1:
         selected_alphabet = alphabet_spanish
         selected_alphabet_inv = alphabet_spanish_inv
-    elif language_option == 2:
+    else:
         selected_alphabet = alphabet_english
         selected_alphabet_inv = alphabet_english_inv
-    else:
-        print("Invalid language option.")
-        continue
 
     # Validate Operation
     if option == 1:
         result = encryption(message,key,selected_alphabet,selected_alphabet_inv)
         print(f"\nYour encrypted message is: {result}")
-    elif option == 2:
+    else: 
         result = decryption(message,key,selected_alphabet,selected_alphabet_inv)
         print(f"\nYour decrypted message is: {result}")
-    else:
-        print("Invalid operation.")
-        continue
+    
 
     # Ask if wanna do anything more
 
-
-    option_activity = int(input("\nWould you like to do anything else?\n1) Yes\n2) No\nChoose an option: \t"))
-
-    while(option_activity > 2 or option_activity <1): 
-        option_activity =int(input("\nInvalid Option. Remember: \n\n1) Yes\n2) No\n"))
-
+    option_activity = valid_type("\nWould you like to do anything else?\n1) Yes\n2) No\nChoose an option: ",(1,2))
+    
     if option_activity == 2:
         print("Goodbye!")
         break
